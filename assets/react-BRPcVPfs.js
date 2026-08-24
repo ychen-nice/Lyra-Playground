4f764e81b937b1defa59b85794e0c7c0e15c7b31
Yoav Chen <yoav.chen@nice.com>
2026-08-24 16:59:03 +0300

    Update Storybook build with menu close-on-resize fix

--- assets/react-BRPcVPfs.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./DocsRenderer-LL677BLK-Bw3ltgCK.js";t();export{e as MDXProvider};
---
+++ assets/react-Dy8bh3tD.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-Dm8ZABVn.js";t();export{e as MDXProvider};
